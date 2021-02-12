ec655307e57311410153b4bce7f63680e392a384
Pratit Todkar <pratittodkar@gmail.com>
2021-02-13 01:30:38 +0530

    Updated main.py
    Signed-off-by: Pratit Todkar <pratittodkar@gmail.com>

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ class DataHandler:
             json.dump(self.data, self.f)
 
 
-handler = DataHandler()
-handler.view_data()
-handler.add_data(handler.data)
-handler.view_data()
-handler.write_data()
+handler = DataHandler()  # Creates instance of the data handler class
+handler.view_data()  # Reads data from data.json file and prints it
+handler.add_data(handler.data)  # Runs add_data() function to add data
+handler.view_data()  # Runs view_data() function to read and print changes
+handler.write_data()  # Commits changes to data.json file
